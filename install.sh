b101721c03b114a263b8487873eab3b2697a1420
Quang Ngo <quang.ngongoc@hungthinhcorp.com.vn>
2026-07-14 17:29:13 +0700

    first commit

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+
+echo "=== Update system ==="
+apt update && apt upgrade -y
+
+echo "=== Install curl ==="
+apt install -y curl wget ca-certificates gnupg lsb-release
+
+echo "=== Install OpenSSH ==="
+apt install -y openssh-server
+
+echo "=== Enable SSH ==="
+systemctl enable ssh
+systemctl restart ssh
+
+echo "=== Enable root login + password auth ==="
+
+mkdir -p /etc/ssh/sshd_config.d
+
+cat > /etc/ssh/sshd_config.d/99-custom.conf <<EOF
+PermitRootLogin yes
+PasswordAuthentication yes
+PubkeyAuthentication yes
+EOF
+
+sshd -t
+systemctl restart ssh
+
+
+echo "=== Install Docker ==="
+curl -fsSL https://get.docker.com | sh
+
+systemctl enable docker
+systemctl start docker
+
+echo "=== Install NVM ==="
+export NVM_VERSION="v0.40.5"
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh | bash
+
+
+echo ""
+echo "==================================="
+echo "DONE"
+echo "==================================="
+echo ""
